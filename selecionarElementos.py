2bdacc57348d8b89f084c573310ba0d26efbc01f
renatoguara2020 <renatoguara2020@gmail.com>
2024-12-26 11:01:16 -0300

    ok

--- selecionarElementos.py
+++ selecionarElementos.py
@@ -1,7 +1,7 @@
 """
 find_element(By.ID, "id")
 find_element(By.NAME, "name")
-find_element(By.XPATH, "xpath")
+find_element(By.XPATH, "xpath") (MAIS COMPLEXO, MAIS PRECISO)
 find_element(By.LINK_TEXT, "link text")
 find_element(By.PARTIAL_LINK_TEXT, "partial link text")
 find_element(By.TAG_NAME, "tag name")
